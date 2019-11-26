e057846a46749a884e67c35bc847fada67235ce3
Andres Felipe Montealegre Olaya <afmontealegreol@unal.loc>
2019-11-26 17:36:28 -0500

    Fixed error in changing role to agent_role

--- app_1_market/models.py
+++ app_1_market/models.py
@@ -31,10 +31,10 @@ class Subsession(BaseSubsession):
 
         # loading roles:
         if self.round_number == 1:
-            role = itertools.cycle([False, True])
+            agent_role = itertools.cycle([False, True])
             for p in self.get_players():
-                p.role = next(role)
-                p.participant.vars['role'] = p.role
+                p.agent_role = next(agent_role)
+                p.participant.vars['agent_role'] = p.agent_role
 
         # loading valuiations:
         #seller_valuations = [7, 6, 5, 5, 4, 4, 3, 2, 1, 1]
@@ -46,11 +46,11 @@ class Subsession(BaseSubsession):
         buyer_valuations_pac4 = itertools.cycle([10, 10, 9, 8, 8, 7, 6, 6, 5, 4])
         buyer_valuations_pac5 = itertools.cycle([10, 10, 9, 8, 8, 7, 6, 6, 5, 4])
         for p in self.get_players():
-            if p.role == False:
+            if p.agent_role == False:
                 #p.seller_valuation = numpy.random.choice(seller_valuations, replace=False)
                 p.seller_valuation = next(seller_valuations)
                 p.participant.vars['seller_valuation'] = p.seller_valuation
-            elif p.role == True:
+            elif p.agent_role == True:
                 #p.buyer_valuation = numpy.random.choice(buyer_valuations, replace=False)
                 p.buyer_valuation_pac1 = next(buyer_valuations_pac1)
                 p.buyer_valuation_pac2 = next(buyer_valuations_pac2)
@@ -65,14 +65,14 @@ class Subsession(BaseSubsession):
         # loading packages:
         packages = [1, 2, 3, 4, 5]
         for p in self.get_players():
-            if p.role == False:
+            if p.agent_role == False:
                 p.seller_package = numpy.random.choice(packages, replace=False)
                 p.participant.vars['seller_package'] = p.seller_package
 
 
 #        print("[[ APP_1_MARKET]] - PLAYER - CREATING_SESSION.............[[[ ROUND NUMBER ==> ", self.round_number, " <== ]]")
 #        print("[[ APP_1_MARKET]] - PLAYER - CREATING_SESSION.............[[[ PLAYER_ID ==> ", self.id_in_group, " <== ]]")
-#        print("[[ APP_1_MARKET]] - PLAYER - CREATING_SESSION.............[[[ ROLE ==> ", self.participant.vars['role'], " <== ]]")
+#        print("[[ APP_1_MARKET]] - PLAYER - CREATING_SESSION.............[[[ AGENT_ROLE ==> ", self.participant.vars['agent_role'], " <== ]]")
 #        print("[[ APP_1_MARKET]] - PLAYER - CREATING_SESSION.............[[[ SELLER_VALUATION ==> ", self.participant.vars['seller_valuation'], " <== ]]")
 #        print("[[ APP_1_MARKET]] - PLAYER - CREATING_SESSION.............[[[ BUYER_VALUATION ==> ", self.participant.vars['buyer_valuation'], " <== ]]")
 
