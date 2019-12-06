450f2efc3da5f729d2d98bbe510404cf359d7135
rafaelcharris <rafaelcharris92@gmail.com>
2019-12-06 11:41:06 -0500

    agregar c() para usar los puntos

--- app_1_market/models.py
+++ app_1_market/models.py
@@ -23,8 +23,8 @@ class Constants(BaseConstants):
     name_in_url = 'app_1_market'
     players_per_group = 4
     num_rounds = 5
-    endowment = 30
-    see_list_cost = 3
+    endowment = c(30)
+    see_list_cost = c(3)
 
     packages = [i for i in range(1, 6)]
     id = itertools.cycle([i for i in range(1,11)])
@@ -52,7 +52,7 @@ class Subsession(BaseSubsession):
                 if p.participant.vars['role'] == 'seller':
                     p.seller_package = numpy.random.randint(1, 5)
                     p.participant.vars['seller_package'] = p.seller_package
-                    p.seller_valuation = numpy.random.choice(Constants.seller_valuations, replace=False)
+                    p.seller_valuation = c(numpy.random.choice(Constants.seller_valuations, replace=False))
                     p.seller_id = next(Constants.id)
                     #todo have to fix this id. They don't work as I would like it to
                     p.participant.vars['seller_id'] = p.seller_id #assign a seller id
@@ -60,7 +60,7 @@ class Subsession(BaseSubsession):
                 else:
             # Assign valuations for each packaque for the sellers
             #Esta parte del código debería asignarle un valor random a cada paquete
-                    p.participant.vars["valuations"] = dict(zip(Constants.packages, numpy.random.choice(Constants.buyer_valuations, size =5, replace = False)))
+                    p.participant.vars["valuations"] = dict(zip(Constants.packages, c(numpy.random.choice(Constants.buyer_valuations, size =5, replace = False))))
                     p.buyer_valuation_pac1 = p.participant.vars["valuations"].get(1)
                     p.buyer_valuation_pac2 = p.participant.vars["valuations"].get(2)
                     p.buyer_valuation_pac3 = p.participant.vars["valuations"].get(3)
@@ -89,9 +89,6 @@ class Group(BaseGroup):
             else:
 
                 p.payoff = (p.ask_price_fin - p.seller_valuation - int(p.see_list)*Constants.see_list_cost)*int(p.sold)
-
-    #Y si hago que esta función corra cuando hacen click. conectando con la función de javascript?
-
 
 class Player(BasePlayer):
 
