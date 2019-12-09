c0d7c3a4167d85c341f4d0a0c58f5775b0f5eb98
rafaelcharris <rafaelcharris92@gmail.com>
2019-12-09 14:44:28 -0500

    agregar limite de tiempo a informal sanction

--- app_2_informal_sanction/pages.py
+++ app_2_informal_sanction/pages.py
@@ -50,11 +50,14 @@ class seller_2(Page):
 class buyer(Page):
     form_model = 'player'
     form_fields = ['my_seller'] #la idea es que como tengo la id en group, puedo recuperar qué estaba vendiendo y a cómo.
+    timeout_seconds = 60 #tiempo para que la página pase
 
     def is_displayed(self):
         return self.player.role() != 'seller'
 
     def vars_for_template(self):
+        import time
+        self.player.time_spent = time.time()
         return dict(
             role = self.participant.vars['role'],
             pac_val = self.participant.vars['valuations'],
