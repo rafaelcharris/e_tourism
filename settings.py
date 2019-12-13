75bdb5f52f13da31e1ba57565fbf2a1dcb4c6702
Felipe Montealegre <43189041+felmola@users.noreply.github.com>
2019-12-13 12:24:37 -0500

    Deleted admin password entry

--- settings.py
+++ settings.py
@@ -77,7 +77,7 @@ ROOMS = [
 
 ADMIN_USERNAME = 'admin'
 # for security, best to set admin password in an environment variable
-ADMIN_PASSWORD = environ.get('OTREE_ADMIN_PASSWORD')
+#ADMIN_PASSWORD = environ.get('OTREE_ADMIN_PASSWORD')
 
 DEMO_PAGE_INTRO_HTML = """ """
 
