d0dafb80936c9cb62032edf80a470b8b519e3347
rafaelcharris <rafaelcharris92@gmail.com>
2019-12-12 21:57:17 -0500

    agregar quien es el comprador

--- app_2_market_control/models.py
+++ app_2_market_control/models.py
@@ -84,6 +84,7 @@ class Group(BaseGroup):
                 if p.my_seller > 0:
                     the_seller = self.get_player_by_id(p.my_seller)
                     the_seller.sold = True
+                    the_seller.my_buyer = p.id_in_group
                     p.package_purchased = the_seller.seller_package
                     p.paid = the_seller.ask_price_fin
                     p.payoff = int(Constants.endowment)
@@ -174,7 +175,7 @@ class Player(BasePlayer):
 
     seller_id = models.IntegerField()
     sold = models.BooleanField(initial = False)
-
+    my_buyer = models.IntegerField(initial = 0)
     #Buyer
     #Preguntar a Felipe si puedo borrar estos campos de valuación de cada paquete
     buyer_valuation_pac1 = models.IntegerField()
